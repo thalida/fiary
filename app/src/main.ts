40288ac2f1a937c191e4281dd506248529091a7d
Thalida Noel <thalida.c.noel@gmail.com>
2022-09-16 13:54:47 +0000

    Updated deps; Removed unused deps;

--- app/src/main.ts
+++ app/src/main.ts
@@ -1,6 +1,5 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
-import VueKonva from 'vue-konva';
 
 import App from "./App.vue";
 import router from "./router";
@@ -13,6 +12,5 @@ const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
-app.use(VueKonva);
 
 app.mount("#app");
