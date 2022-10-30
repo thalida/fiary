b457bcca4e202a37a810fafe1dfc9771be5cadb5
Thalida Noel <thalida.c.noel@gmail.com>
2022-10-30 20:58:21 +0000

    Add token delete

--- api/users/schema.py
+++ api/users/schema.py
@@ -56,4 +56,8 @@ class UserMutation(graphene.ObjectType):
     token_auth = graphql_jwt.relay.ObtainJSONWebToken.Field()
     verify_token = graphql_jwt.relay.Verify.Field()
     refresh_token = graphql_jwt.relay.Refresh.Field()
+    delete_token_cookie = graphql_jwt.relay.DeleteJSONWebTokenCookie.Field()
+    revoke_token = graphql_jwt.relay.Revoke.Field()
+    delete_refresh_token_cookie = graphql_jwt.relay.DeleteRefreshTokenCookie.Field()
+
     register = Register.Field()
